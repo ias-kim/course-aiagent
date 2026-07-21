a3ef3b264dbe85b9ae594b3b5e8085154c3c3df5
Theunkilabledemonking <abcqkdnxm@g.yju.ac.kr>
2026-07-21 10:25:42 +0900

    coroutine object code

--- test/test.py
+++ test/test.py
@@ -1,7 +1,16 @@
 import asyncio
 
+async def call_llm(promt:str)->str:
+    print(f"prompt: {promt}")
+    await asyncio.sleep(2)
+    return f"end prompt: {promt}"
+
 async def main():
-    ...
+    print("hello world")
+    result = await call_llm("한국의 수도는?")
+    print(result)
+
+    print("프로그램 종료")
 
 if __name__ == "__main__":
     asyncio.run(main())
